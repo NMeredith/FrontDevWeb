a6a25ce5598abefbbdba979c2654b04a9b238dc8
Meredith Nolin <meredith.nolin9@etu.univ-lorraine.fr>
2022-03-09 11:35:05 +0100

    init commit

---
+++ src/app/model/Card.ts
@@ -0,0 +1,12 @@
+export interface Card {
+  id?: number;
+  name?: string;
+  manaCost?: string;
+  convertedManaCost?: number;
+  colors?: string;
+  type?: string[];
+  text?: string;
+  power?: string;
+  toughness?: string;
+  imageUrl?: string | ArrayBuffer | null;
+}
